fc28007ed873a571d84b8012f420900638876fc0
Arman Sidhu <61654102+armansidhu3@users.noreply.github.com>
2021-01-17 21:42:35 +0530

    Add files via upload

---
+++ sb-hacksupdate/build/precache-manifest.974f4eeed924fe8b0e91a0e94ed3bb7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f95e38ad6622d4daa8e96b922b8613d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89514bb0c006c0d862a8",
+    "url": "/static/css/main.fb8a0c7a.chunk.css"
+  },
+  {
+    "revision": "d8563fceae0988584cb7",
+    "url": "/static/js/2.bcf675db.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.bcf675db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89514bb0c006c0d862a8",
+    "url": "/static/js/main.a1f3c904.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
